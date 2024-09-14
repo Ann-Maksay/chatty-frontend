28b900e330667429b2aeeaffb21010504f5bf54a
Anna Maksai <annamaksai99@gmail.com>
2024-09-14 14:51:55 +0300

    feat: react-hook-form, input component, input style, icons for auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-import { Button } from "~/components/components";
+import { Button, Input } from "~/components/components";
 
 import "~/assets/css/main.css";
 
@@ -22,23 +23,43 @@ function App() {
     fetchData();
   }, [apiUrl]);
 
+  const { control } = useForm({
+    defaultValues: {
+      value: "",
+    },
+    mode: "onChange",
+  });
+
   return (
     <>
       <h1>Chatty</h1>
-
       {/* all this data is only for testing purposes and may be deleted or commented on as needed */}
 
+      {/* api test */}
       {/* <div>
         <h1>API Data:</h1>
         <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      </div> */}
 
-      <div className="testContainer testContainer--white">
+      {/* button test */}
+      {/* <div className="testContainer testContainer--white">
         <Button label="Send" style="primary" />
       </div>
       <div className="testContainer">
         <Button label="Sign in" style="welcome" />
       </div> */}
+
+      {/* inputs test */}
+
+      <form className="testContainer testContainer--white">
+        <Input
+          control={control}
+          label="Test Input"
+          icon="user"
+          name="value"
+          placeholder="Nickname"
+        />
+      </form>
     </>
   );
 }
---
+++ src/components/input/input.tsx
@@ -0,0 +1,51 @@
+import {
+  type Control,
+  type FieldPath,
+  type FieldValues,
+  useController,
+} from "react-hook-form";
+
+import { StyledInput, StyledLabel, StyledWrapper } from "./input.styles";
+
+import { type IconName } from "~/types/icon-name.type";
+
+type Properties<T extends FieldValues> = {
+  control: Control<T, null>;
+  isRequired?: boolean;
+  icon?: IconName;
+  inputMode?: "email" | "text";
+  label: string;
+  name: FieldPath<T>;
+  placeholder?: string;
+  style?: "primary";
+  type?: "text";
+};
+
+const Input = <T extends FieldValues>({
+  control,
+  isRequired = false,
+  icon = "none",
+  inputMode = "text",
+  label,
+  name,
+  placeholder,
+  style = "primary",
+  type = "text",
+}: Properties<T>): JSX.Element => {
+  const { field } = useController({ control, name });
+
+  return (
+    <StyledWrapper $icon={icon} $style={style}>
+      <StyledLabel>{label}</StyledLabel>
+      <StyledInput
+        placeholder={placeholder}
+        required={isRequired}
+        inputMode={inputMode}
+        type={type}
+        {...field}
+      />
+    </StyledWrapper>
+  );
+};
+
+export { Input };
