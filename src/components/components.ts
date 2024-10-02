45d6f2b955ea05ac5751d5d0d30ce425c625e9dc
Anna Maksai <annamaksai99@gmail.com>
2024-10-02 17:30:36 +0300

    feat: remove test code, add react-router, add routes for auth and welcome

--- src/components/components.ts
+++ src/components/components.ts
@@ -2,3 +2,4 @@ export { Button } from "./button/button";
 export { Input } from "./input/input";
 export { Link } from "./link/link";
 export { Checkbox } from "./checkbox/checkbox";
+export { RouterProvider } from "./router-provider/router-provider";
